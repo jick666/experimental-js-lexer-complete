eef23410a7533a25e58149f4214d0f7304eef860
jick666 <34618290+jick666@users.noreply.github.com>
2025-06-17 15:48:23 -0800

    perf: improve tokenization speed

--- src/lexer/WhitespaceReader.js
+++ src/lexer/WhitespaceReader.js
@@ -3,8 +3,10 @@
  * Consumes consecutive whitespace characters.
  * Does not emit a token; callers may attach trivia if needed.
  */
+const WHITESPACE = new Set([' ', '\n', '\t', '\r', '\v', '\f']);
+
 export function WhitespaceReader(stream) {
-  while (!stream.eof() && /\s/.test(stream.current())) {
+  while (!stream.eof() && WHITESPACE.has(stream.current())) {
     stream.advance();
   }
   // No token is returned for whitespace.
