de35685623b6cb217d50e7728ab40c4166916503
jick666 <34618290+jick666@users.noreply.github.com>
2025-06-19 09:31:13 -0800

    feat: add rich token metadata

--- tests/token.test.js
+++ tests/token.test.js
@@ -4,15 +4,24 @@ test("Token.toJSON produces plain object", () => {
   const tok = new Token(
     "NUMBER",
     "1",
-    { line: 1, column: 0 },
-    { line: 1, column: 1 }
+    { line: 1, column: 0, index: 0, sourceURL: "test.js" },
+    { line: 1, column: 1, index: 1, sourceURL: "test.js" }
   );
   const expected = {
     type: "NUMBER",
     value: "1",
-    start: { line: 1, column: 0 },
-    end: { line: 1, column: 1 }
+    start: { line: 1, column: 0, index: 0, sourceURL: "test.js" },
+    end: { line: 1, column: 1, index: 1, sourceURL: "test.js" },
+    range: [0, 1],
+    sourceURL: "test.js"
   };
   expect(tok.toJSON()).toEqual(expected);
   expect(JSON.parse(JSON.stringify(tok))).toEqual(expected);
+});
+
+import { tokenize } from '../index.js';
+
+test('tokenize includes sourceURL in tokens', () => {
+  const toks = tokenize('let a = 1;', { sourceURL: 'foo.js' });
+  expect(toks[0].sourceURL).toBe('foo.js');
 });
